89d27731dd84a417dc3823ca5d3f0b65acef9ad4
Motea Marius <marius.motea@gmail.com>
2017-09-03 17:34:20 +0300

    improve xy color conversion (#80)

--- ESP8266HueEmulator/LightService.cpp
+++ ESP8266HueEmulator/LightService.cpp
@@ -795,23 +795,69 @@ void sendJson(aJsonObject *root)
 // The code is based on this brilliant note: https://github.com/PhilipsHue/PhilipsHueSDK-iOS-OSX/commit/f41091cf671e13fe8c32fcced12604cd31cceaf3
 
 rgbcolor getXYtoRGB(float x, float y, int brightness_raw) {
-  float brightness = ((float)brightness_raw) / 255.0f;
-  float bright_y = brightness / y;
-  float X = x * bright_y;
-  float Z = (1 - x - y) * bright_y;
+  float Y = brightness_raw / 250.0f;
 
-  // convert to RGB (0.0-1.0) color space
-  float R = X * 1.4628067 - brightness * 0.1840623 - Z * 0.2743606;
-  float G = -X * 0.5217933 + brightness * 1.4472381 + Z * 0.0677227;
-  float B = X * 0.0349342 - brightness * 0.0968930 + Z * 1.2884099;
+  float z = 1.0f - x - y;
+  float X = (Y / y) * x;
+  float Z = (Y / y) * z;
 
-  // apply inverse 2.2 gamma
-  float inv_gamma = 1.0 / 2.4;
-  float linear_delta = 0.055;
-  float linear_interval = 1 + linear_delta;
-  float r = R <= 0.0031308 ? 12.92 * R : (linear_interval) * pow(R, inv_gamma) - linear_delta;
-  float g = G <= 0.0031308 ? 12.92 * G : (linear_interval) * pow(G, inv_gamma) - linear_delta;
-  float b = B <= 0.0031308 ? 12.92 * B : (linear_interval) * pow(B, inv_gamma) - linear_delta;
+  // sRGB D65 conversion
+  float r =  X * 1.656492f - Y * 0.354851f - Z * 0.255038f;
+  float g = -X * 0.707196f + Y * 1.655397f + Z * 0.036152f;
+  float b =  X * 0.051713f - Y * 0.121364f + Z * 1.011530f;
+
+  if (r > b && r > g && r > 1.0f) {
+    // red is too big
+    g = g / r;
+    b = b / r;
+    r = 1.0f;
+  }
+  else if (g > b && g > r && g > 1.0f) {
+    // green is too big
+    r = r / g;
+    b = b / g;
+    g = 1.0f;
+  }
+  else if (b > r && b > g && b > 1.0f) {
+    // blue is too big
+    r = r / b;
+    g = g / b;
+    b = 1.0f;
+  }
+
+  // Apply gamma correction
+  r = r <= 0.0031308f ? 12.92f * r : (1.0f + 0.055f) * pow(r, (1.0f / 2.4f)) - 0.055f;
+  g = g <= 0.0031308f ? 12.92f * g : (1.0f + 0.055f) * pow(g, (1.0f / 2.4f)) - 0.055f;
+  b = b <= 0.0031308f ? 12.92f * b : (1.0f + 0.055f) * pow(b, (1.0f / 2.4f)) - 0.055f;
+
+  if (r > b && r > g) {
+    // red is biggest
+    if (r > 1.0f) {
+      g = g / r;
+      b = b / r;
+      r = 1.0f;
+    }
+  }
+  else if (g > b && g > r) {
+    // green is biggest
+    if (g > 1.0f) {
+      r = r / g;
+      b = b / g;
+      g = 1.0f;
+    }
+  }
+  else if (b > r && b > g) {
+    // blue is biggest
+    if (b > 1.0f) {
+      r = r / b;
+      g = g / b;
+      b = 1.0f;
+    }
+  }
+
+  r = r < 0 ? 0 : r;
+  g = g < 0 ? 0 : g;
+  b = b < 0 ? 0 : b;
 
   return rgbcolor(r * COLOR_SATURATION,
                   g * COLOR_SATURATION,
@@ -903,7 +949,7 @@ bool parseHueLightInfo(HueLightInfo currentInfo, aJsonObject *parsedRoot, HueLig
       newInfo->effect = EFFECT_NONE;
     }
   }
-  
+
   // pull alert
   aJsonObject* alertState = aJson.getObjectItem(parsedRoot, "alert");
   if (alertState) {
@@ -1237,4 +1283,3 @@ String methodToString(int method) {
     default: return "unknown";
   }
 }
-
